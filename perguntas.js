b916800626bd350d38ba76babfbd7931f966070e
Giovanafabro <135335861+Giovanafabro@users.noreply.github.com>
2024-11-27 15:15:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Cantora',
+    'Quem é Priscila Alcantâra?',
+    'Priscilla Alcantara Silva Fonseca, mais conhecida como Priscilla é uma cantora, compositora, multi-instrumentista, apresentadora, dubladora, escritora, atriz e produtora musical brasileira. '
+)
+
+criaCartao(
+   'Nelson Mandela',
+   'Quem foi Nelson Mandela?,
+  'Nelson Rolihlahla Mandela foi um advogado, líder rebelde e presidente da África do Sul de 1994 a 1999, considerado como o mais importante líder da África Subsaariana, vencedor do Prêmio Nobel da Paz de 1993, e pai da moderna nação sul-africana, onde é normalmente referido como Madiba ou "Tata'
+ 
+)
+
+criaCartao(
+    'Vulcões',
+    'Como surgem os vulcões?',
+    'Os vulcões surgem quando as chamadas placas tectônicas que fazem parte da crosta terrestre se chocam movimentando o material presente sobre elas e deixando aberturas para camadas mais profundas do planeta. Por essas aberturas pode sair o magma presente entre a crosta e a manta camada média da Terra?',
+    
+)
+criaCartao(
+    'Qual a montanha mais alta do mundo?',
+    'O Monte Everest tem 8.848 metros de altitude e localiza-se no Nepal, um país asiático que faz fronteira com a China e com a Índia.'
+)
+
+    criaCartao(
+        'Onde se localiza Machu Picchu?',
+        'Machu Picchu localiza-se no Peru, país da América do Sul que faz fronteira com o Brasil.'
+    )
+
+    criaCartao(
+        'Que país tem o formato de uma bota',
+        'No mapa da Europa, a Itália se destaca por parecer uma bota de cano e salto altos.'
+
+
+        criaCartao(
+            'Quem inventou a lâmpada',
+            'A lâmpada foi inventada por Thomas Edison (1847-1931) em 1879. No dia 21 de outubro, o inventor conseguiu manter uma lâmpada acessa durante 48 horas.'
+
+
+       criaCartao(
+            ' A que temperatura a água ferve'?
+            'A água ferve a 100 ºC é conhecido como o ponto de ebulição da água'
+ 
+
+         criaCartao(
+                'Quais são as fases da Lua'?
+                'As fases da lua são quatro: nova, crescente, cheia e minguante. Cada uma delas dura 7 ou 8 dias.'
+
+
+                criaCartao(
+                'Qual o maior planeta do sistema solar?',
+                'Júpiter é 1300 vezes maior que a Terra, sendo o maior planeta do sistema solar.'
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Cores
+
+```css
+:root {
+    --text-color: F1F0E8;
+    --card-front-color: #124480;
+    --card-back-color: #98F443;
+}
+```
+
+> Para mais cores
+
+- https://colorhunt.co/
+- https://color.adobe.com/pt/create/color-wheel
+
+## Para fontes
+https://fonts.google.com/
